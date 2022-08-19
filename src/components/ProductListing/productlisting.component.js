c6cae34ab5bc65eec972f597344ae53aa74b0915
sanjana <sanjanaprameela@gmail.com>
2022-08-19 12:25:00 -0400

    Cosmetic Changes

--- src/components/ProductListing/productlisting.component.js
+++ src/components/ProductListing/productlisting.component.js
@@ -1,5 +1,5 @@
 
-import Header from "../components/Header/header.component";
+import Header from "../Header/header.component";
 import React from "react";
 import {useNavigate} from 'react-router-dom'
 import axios from "axios";
